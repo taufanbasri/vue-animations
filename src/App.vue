31477780668525e0d05d0cd21c7695f025ff153d
Taufan Basri <taufanprasetyobasri@gmail.com>
2022-06-01 22:35:50 +0700

    Transitioning Multiple Elements

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,12 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -24,10 +30,17 @@ export default {
     return {
       animatedBlock: false,
       dialogIsVisible: false,
-      paragraphIsVisible: false
+      paragraphIsVisible: false,
+      usersAreVisible: false
     };
   },
   methods: {
+    showUsers() {
+      this.usersAreVisible = true
+    },
+    hideUsers() {
+      this.usersAreVisible = false
+    },
     toggleParagraph() {
       this.paragraphIsVisible = !this.paragraphIsVisible
     },
@@ -104,6 +117,24 @@ button:active {
 
 .para-leave {
   animation: slide-fade 0.3s ease-out;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 
 @keyframes slide-fade {
